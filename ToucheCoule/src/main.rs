58285a21998604d72f3d52ec23be6ec4c9d2dd5b
aurirml <auriane.ramel@orange.fr>
2023-09-24 18:16:33 +0900

    fonction pour jouer un coup

--- ToucheCoule/src/main.rs
+++ ToucheCoule/src/main.rs
@@ -1,4 +1,7 @@
+use action::futurcoup;
+
 mod init;
+mod action;
 
 fn main() {
     let _tab_j1int: [i32;100]=[0;100];
@@ -8,4 +11,7 @@ fn main() {
     init::display_tab_int(&_tab_j1int);
     init::display_tab_str(&_tab_j1str);
 
+    let coup = action::futurcoup();
+    let tir = coup(&coup);
+    
 }
